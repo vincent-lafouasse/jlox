137ee01f883466b846d368b1389a8bb3518603c9
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-12-30 12:22:11 +0100

    call scanner from lox

--- com/poss/lox/Lox.java
+++ com/poss/lox/Lox.java
@@ -9,6 +9,8 @@ import java.nio.file.Paths;
 import java.util.List;
 
 class Lox {
+	static boolean hadError = false;
+
 	public static void main(String[] args) throws IOException {
 		if (args.length > 1) {
 			System.out.println("Usage: jlox [script]");
@@ -21,6 +23,12 @@ class Lox {
 	}
 
 	private static void run(String source) {
+		Scanner scanner = new Scanner(source);
+		List<Token> tokens = scanner.scanTokens();
+
+		for (Token token : tokens) {
+			System.out.println(token);
+		}
 	}
 
 	private static void runPrompt() throws IOException {
@@ -32,11 +40,22 @@ class Lox {
 			String line = reader.readLine();
 			if (line == null) break;
 			run(line);
+			hadError = false;
 		}
 	}
 
 	private static void runFile(String path) throws IOException {
 		byte[] bytes = Files.readAllBytes(Paths.get(path));
 		run(new String(bytes, Charset.defaultCharset()));
+		if (hadError) System.exit(65);
+	}
+
+	static void error(int line, String message) {
+		report(line, "", message);
+	}
+
+	private static void report(int line, String where, String message) {
+		System.err.println("[line " + line + "] Error" + where + ": " + message);
+		hadError = true;
 	}
 }
