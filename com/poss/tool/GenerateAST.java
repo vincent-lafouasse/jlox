b1548a341d2a3cf8151b6b3d905bc0dda2af8f5b
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-12-30 13:12:04 +0100

    iplement generator

--- com/poss/tool/GenerateAST.java
+++ com/poss/tool/GenerateAST.java
@@ -13,6 +13,20 @@ class GenerateAST {
 		}
 
 		String outputDir = args[0];
-		System.out.println(outputDir);
+	}
+
+	private static void defineAst( String outputDir, String baseName,
+		List<String> types) throws IOException {
+		String path = outputDir + "/" + baseName + ".java";
+		PrintWriter writer = new PrintWriter(path, "UTF-8");
+
+		writer.println("package com.poss.lox;");
+		writer.println();
+		writer.println("import java.util.List;");
+		writer.println();
+		writer.println("abstract class " + baseName + " {");
+
+		writer.println("}");
+		writer.close();
 	}
 }
