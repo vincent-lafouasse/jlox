059c22a291d01e320af74013fa77e98ecf657d22
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-12-30 12:05:09 +0100

    build

--- com/poss/lox/Lox.java
+++ com/poss/lox/Lox.java
@@ -1,5 +1,18 @@
+package com.poss.lox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 class Lox {
-	public static void main(String[] args) {
-		System.out.println("Hello"); 
+	public static void main(String[] args) throws IOException {
+		if (args.length > 1) {
+			System.out.println("Usage: jlox [script]");
+			System.exit(64);
+		}
 	}
 }
--- com/poss/tool/GenerateAST.java
+++ com/poss/tool/GenerateAST.java
@@ -1,3 +1,5 @@
+package com.poss.tool;
+
 class GenerateAST {
 	public static void main(String[] args) {
 		System.out.println("Hello also"); 
