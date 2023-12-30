6a39e3bda562039d00de01191e4c88673672440a
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-12-30 13:08:41 +0100

    generate ast

--- com/poss/tool/GenerateAST.java
+++ com/poss/tool/GenerateAST.java
@@ -1,6 +1,18 @@
 package com.poss.tool;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
+
 class GenerateAST {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		if (args.length != 1) {
+			System.err.println("Usage: generate_ast <output directory>");
+			System.exit(64);
+		}
+
+		String outputDir = args[0];
+		System.out.println(outputDir);
 	}
 }
